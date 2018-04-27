3f65b9485426124bc6a848df8dfac41c54ddf9c0
DarkSide4 <37451968+DarkSide4@users.noreply.github.com>
2018-04-26 20:09:26 -0600

    Acomodeo los botones del teclado

--- src/sample/teclado.java
+++ src/sample/teclado.java
@@ -5,23 +5,23 @@ import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.GridPane;
-import javafx.scene.layout.Priority;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.*;
 import javafx.stage.Stage;
 
 public class teclado extends Application {
     @Override
     public void start(Stage primaryStage) {
         AnchorPane anchor = new AnchorPane();
+        anchor.setStyle("-fx-background-color:#5c686d");
         BorderPane root = new BorderPane();
-        AnchorPane.setBottomAnchor(root, 0.0);
+        VBox acomodar = new VBox();
+        AnchorPane.setBottomAnchor(root, 10.0);
         AnchorPane.setTopAnchor(root, 0.0);
         AnchorPane.setLeftAnchor(root, 0.0);
         AnchorPane.setRightAnchor(root, 0.0);
         anchor.getChildren().add(root);
-        Scene scene = new Scene(anchor, 800, 600);
+        Scene scene = new Scene(anchor, 650, 700);
 
 
         String[] keys =
@@ -37,7 +37,7 @@ public class teclado extends Application {
             Button button = new Button(keys[i]);
             button.getStylesheets().add("thisnuts.css");
             button.getStyleClass().add("Teclado");
-            button.setPrefSize(130, 200);
+            button.setPrefSize(200, 200);
             button.setPadding(new Insets(40, 40, 40, 40));
 
             numPad.add(button, i % 3, (int) Math.ceil(i / 3));
@@ -47,15 +47,18 @@ public class teclado extends Application {
         numPad.setAlignment(Pos.CENTER);
         Button call = new Button("Regresar");
         call.setId("call-button");
-        call.setPrefSize(390, 100); //width es el triple de button.setPrefSize
+        call.setPrefSize(600, 100); //width es el triple de button.setPrefSize
         //  call.setMaxSize(Double.MAX_VALUE, Double.MAX_VALUE);
         numPad.add(call, 0, 4);
 
         GridPane.setColumnSpan(call, 3);
         GridPane.setHgrow(call, Priority.ALWAYS);
 
-        root.setCenter(numPad);
-
+        root.setCenter(acomodar);
+        acomodar.getChildren().addAll(numPad,call);
+        acomodar.setAlignment(Pos.CENTER);
+        acomodar.setSpacing(5);
+        primaryStage.setResizable(false);
         primaryStage.setScene(scene);
         primaryStage.show();
     }
